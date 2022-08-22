34c2beb864b70a7f19d47275f24f86cbe6ce6c2f
nhm204 <19521862@gm.uit.edu.vn>
2022-08-23 03:05:59 +0700

    done header, banner and add movie list

---
+++ components/Header.tsx
@@ -0,0 +1,45 @@
+import { BellIcon, SearchIcon } from '@heroicons/react/solid';
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+
+const Header = () => {
+  const [ isScrolled, setIsScrolled ] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      window.scrollY > 0 ? setIsScrolled(true) : setIsScrolled(false)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, []);
+
+  
+  return (
+    <header className={`${isScrolled && 'bg-[#141414]'}`}>
+      <div className='flex items-center space-x-2 md:space-x-10'>
+        <img src='https://rb.gy/ulxxee' width={100} height={100} className='cursor-pointer object-contain' />
+        <ul className="hidden space-x-4 md:flex">
+          <li className="header-link cursor-default font-semibold text-white hover:text-white">Home</li>
+          <li className="header-link">TV Shows</li>
+          <li className="header-link">Movies</li>
+          <li className="header-link">New &amp; Popular</li>
+          <li className="header-link">My List</li>
+        </ul>
+      </div>
+      <div className="flex items-center space-x-4 text-sm">
+        <SearchIcon className="sm hidden h-6 w-6 sm:inline" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="h-6 w-6" />
+        <Link href="/account">
+          <img src="https://rb.gy/g1pwyx" alt="" className="cursor-pointer rounded" />
+        </Link>
+      </div>
+    </header>
+  )
+};
+
+export default Header;
