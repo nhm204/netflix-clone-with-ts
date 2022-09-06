1e175e6fb35260603fea594497d85c022327a669
nhm204 <19521862@gm.uit.edu.vn>
2022-09-06 23:20:33 +0700

    setup subscription

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,10 +47,13 @@ const Home: React.FC<Props> = (props) => {
   const showModal = useRecoilValue(modalState);
   const movie = useRecoilValue(movieState);
   const list = useList(user?.uid);
+  const subscription = false 
 
   const netflixMovieId = netflixOriginals.map(movie => movie.id);
 
-  if (loading) return null;
+  if (loading || subscription === null ) return null;
+
+  if (!subscription) return <div>Plans</div>
 
   return (
     <div className='relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] w-full'>
